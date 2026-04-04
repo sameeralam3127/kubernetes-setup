5b2c9b2697b6a67bc83c6ac0e4fff5fbc3501b69
Sameer Alam <sameeralam3127@gmail.com>
2026-04-05 00:13:45 +0530

    Refactor app routes and update deployment image version to v2

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
+healthy = True
+
 @app.route("/")
 def home():
-    return "Hello from Kubernetes v1 🚀"
+    return "App is running 🚀"
 
-@app.route("/crash")
-def crash():
-    import os
-    os._exit(1)
+@app.route("/health")
+def health():
+    if healthy:
+        return "OK", 200
+    else:
+        return "NOT OK", 500
+
+@app.route("/break")
+def break_app():
+    global healthy
+    healthy = False
+    return "App is now unhealthy 💥"
+
+@app.route("/fix")
+def fix_app():
+    global healthy
+    healthy = True
+    return "App fixed ✅"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
